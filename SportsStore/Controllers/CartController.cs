561b02066184fb481dcb0ae98977eb6b766cc590
mdkChaos <mdkchaos1@gmail.com>
2019-06-27 12:12:58 +0200

    Add Index.cshtml for CartController

--- SportsStore/Controllers/CartController.cs
+++ SportsStore/Controllers/CartController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Http;
 using SportsStore.Models;
+using SportsStore.Models.ViewModels;
 using SportsStore.Infrastructure;
 using System.Linq;
 
@@ -12,6 +13,15 @@ namespace SportsStore.Controllers
         public CartController(IProductRepository repository)
         {
             _repository = repository;
+        }
+
+        public ViewResult Index(string returnUrl)
+        {
+            return View(new CartIndexViewModel
+            {
+                Cart = GetCart(),
+                ReturnUrl = returnUrl
+            });
         }
 
         public RedirectToActionResult AddToCart(int productId, string returnUrl)
