de0bf845fa208a4599274a4a8cc5abcd97e2f8a5
mdkChaos <mdkchaos1@gmail.com>
2019-06-29 20:14:42 +0200

    Add Administration

--- SportsStore/Controllers/OrderController.cs
+++ SportsStore/Controllers/OrderController.cs
@@ -15,6 +15,20 @@ namespace SportsStore.Controllers
             _cart = cartService;
         }
 
+        public ViewResult List() => View(_repository.Orders.Where(o => !o.Shipped));
+
+        public IActionResult MarkShipped (int orderID)
+        {
+            Order order = _repository.Orders
+                .FirstOrDefault(o => o.OrderID == orderID);
+            if (order != null)
+            {
+                order.Shipped = true;
+                _repository.SaveOrder(order);
+            }
+            return RedirectToAction(nameof(List));
+        }
+
         public ViewResult Checkout() => View(new Order());
 
         [HttpPost]
